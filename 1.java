5bba871f4d1718f956a08ec01ffb00927177a4a0
xuexi007 <44258352+xuexi007@users.noreply.github.com>
2019-09-18 14:01:36 +0800

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,7 +1,7 @@
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//*********************************************
 @SpringBootApplication
 public class ConsumerAppStart {
 
