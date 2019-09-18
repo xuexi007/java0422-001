6d515548e8a554fc675a0d5fd09e2fc8e26c5e06
sunny <test@qq.com>
2019-09-18 14:05:02 +0800

    edit 1.java

--- 1.java
+++ 1.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConsumerAppStart {
 
+   //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
     public static void main(String[] args) {
         SpringApplication.run(ConsumerAppStart.class,args);
     }
